54921401fec73e5206226298ad54f5951eeb7563
Fadila1800016033 <fadila1800016033@webmail.uad.ac.id>
2020-03-30 19:26:24 +0700

    Teknologi Web

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Praktikum1Component } from './pages/praktikum1/praktikum1.component';
+import { Praktikum2Component } from './pages/praktikum2/praktikum2.component';
+import { Praktikum3Component } from './pages/praktikum3/praktikum3.component';
+import { Praktikum4Component } from './pages/praktikum4/praktikum4.component';
+import { Praktikum5Component } from './pages/praktikum5/praktikum5.component';
+import { Praktikum6Component } from './pages/praktikum6/praktikum6.component';
+import { Praktikum7Component } from './pages/praktikum7/praktikum7.component';
+import { Praktikum8Component } from './pages/praktikum8/praktikum8.component';
+import { Praktikum9Component } from './pages/praktikum9/praktikum9.component';
+import { Praktikum10Component } from './pages/praktikum10/praktikum10.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Praktikum1Component,
+    Praktikum2Component,
+    Praktikum3Component,
+    Praktikum4Component,
+    Praktikum5Component,
+    Praktikum6Component,
+    Praktikum7Component,
+    Praktikum8Component,
+    Praktikum9Component,
+    Praktikum10Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
